8e7af1dc8f281a35270dbf6573074585456a52bb
unknown <johnathanthomas331@yahoo.com>
2022-08-27 15:24:51 +0530

    first commit

---
+++ client/build/precache-manifest.01b5dae18d608f3c681c1156d74a2f94.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39651a957e30dfddc789ed8025418957",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cf8582e97f0c3daf9be",
+    "url": "/static/css/main.1edf2ec6.chunk.css"
+  },
+  {
+    "revision": "38e6e295405c1c8d5df2",
+    "url": "/static/js/2.25f416f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.25f416f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cf8582e97f0c3daf9be",
+    "url": "/static/js/main.a14b29ce.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/static/media/cart.8bf1af37.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/static/media/close.2c5552a1.svg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/static/media/menu.b3f49afd.svg"
+  }
+]);
